ca05a6fc80c78bbe930b8fbf3b37fdd6d02bce86
unstoppableThinker <yves.13507@gmail.com>
2024-12-28 22:34:21 -0500

    Branch_04 complete items list page

--- src/components/Filters/Categories.jsx
+++ src/components/Filters/Categories.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const Categories = () => {
+const Categories = ({types}) => {
   return (
-    <div>Categories</div>
+    <div>
+      {types?.map(type => {
+				return (
+					<div className="flex items-center p-1">
+						<input type='checkbox' name={type?.code} className='border rounded-xl w-4 h-4 accent-black text-black' />
+						<label htmlFor={type?.code} className='px-2 text-[14px] text-gray-600'>{type?.name}</label>
+					</div>
+				)
+			})}
+    </div>
   )
 }
 
--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -1,6 +1,11 @@
 import React, { useMemo } from 'react'
 import FilterIcon from '../../components/common/FilterIcon';
 import content from '../../data/content.json';
+import Categories from '../../components/Filters/Categories';
+import PriceFilter from '../../components/Filters/PriceFilter';
+import ColorsFilter from '../../components/Filters/ColorsFilter';
+import SizeFilter from '../../components/Filters/SizeFilter';
+import ProductCard from './ProductCard';
 
 const categories = content?.categories;
 
@@ -10,6 +15,10 @@ const ProductListPage = ({categoryType}) => {
 		return categories?.find((category) => category.code === categoryType);
 	}, [categoryType]);
 
+	const productListItems = useMemo(() => {
+		return content?.products?.filter((product) => product?.category_id === categoryContent?.id);
+	}, [categoryContent]);
+
   return (
     <div>
 			<div className="flex">
@@ -18,13 +27,28 @@ const ProductListPage = ({categoryType}) => {
 						<p className='text-[16px] text-gray-600'>Filter</p>
 						<FilterIcon />
 					</div>
+					<hr className='mt-5'></hr>
 					<div>
 						<p className='text-[16px] text-black mt-5'>Categories</p>
+						<Categories types={categoryContent?.types} />
+						<hr className='mt-5'></hr>
+					</div>
+					<div>
+						<PriceFilter />
+						<hr className='mt-5'></hr>
+						<ColorsFilter colors={categoryContent?.meta_data?.colors} />
+						<hr className='mt-5'></hr>
+						<SizeFilter sizes={categoryContent?.meta_data?.sizes} />
 					</div>
 				</div>
 
 				<div className='p-[15px]'>
 					<p className='text-black text-lg'>{categoryContent?.description}</p>
+					<div className='pt-4 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8 px-2'>
+						{productListItems?.map((item,index)=>(
+							<ProductCard key={item?.id+"_"+index} {...item} title={item?.name}/>
+						))}
+					</div>
 				</div>
 			</div>
     </div>
